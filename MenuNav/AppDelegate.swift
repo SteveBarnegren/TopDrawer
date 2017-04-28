feabd014f354fd94284052f8169dc9c7e0ef05e2
Steve Barnegren <steve.barnegren@gmail.com>
2017-04-28 16:55:44 +0100

    Added status bar button

--- MenuNav/AppDelegate.swift
+++ MenuNav/AppDelegate.swift
@@ -11,14 +11,24 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    //there is a bug in Xcode that prevents it from recognizing NSSquareStatusItemLength, but since it’s just a constant that’s defined as -2, you just use -2 in it’s place
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = #selector(statusBarButtonPressed)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func statusBarButtonPressed() {
+        print("Status bar button pressed")
     }
 
 
